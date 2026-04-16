7ea4a9d97323715d37a45731272d2536ca7c74d1
Arham Awan <arhamawan8584@gmail.com>
2026-04-17 01:50:04 +0500

    Update lkf_asm.s

--- lkf_asm.s
+++ lkf_asm.s
@@ -1,6 +1,6 @@
 # =========================================================================
 #  Linear Kalman Filter - RISC-V RV64GD Assembly
-#  Karan Kumar - 30212 | Team Alpha | Milestone 3
+#  | Team Warjongo | Milestone 3
 #
 #  Architecture:
 #    s0 -> raw sensor data base address
